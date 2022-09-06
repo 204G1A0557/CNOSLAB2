87b7e3fe6ec30a8efb04c21afa7ace00b1109401
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-09-06 11:57:45 +0530

    "A.Megharshini  Created FCFS.C"

---
+++ OSLAB/FCFS.C
@@ -0,0 +1,5 @@
+/*Name Of the Candidate: A.Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022*/
