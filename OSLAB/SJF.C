76eb955fd0bc49b1525e90a95d4cbe3afb4298d5
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-09-06 12:19:19 +0530

    "A.Megharshini created SJF.C"

---
+++ OSLAB/SJF.C
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: A.Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
