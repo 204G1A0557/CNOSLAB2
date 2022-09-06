181c51e155d196676a39fb3bf3dfb1a8c2475e6f
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-09-06 12:38:09 +0530

    A Megharshini Created RR.C

---
+++ OSLAB/RR.C
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A.Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: CPU Scheduling Using RR Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
