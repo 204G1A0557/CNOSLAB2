202bc2b52b3e040e96dd88f0c4758e76145dbcbf
204G1A0557 <204G1A0557@srit.ac.in>
2022-09-20 13:00:47 +0530

    My SJF.C program is completed

--- OSLAB/SJF.C
+++ OSLAB/SJF.C
@@ -8,12 +8,10 @@
 /*source code*/
 
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; 
 float wtavg,tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -47,5 +45,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
+return 0;
 }
