89e1b266a182e188e1105fd943538011746d65e9
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-10-11 11:18:31 +0530

    A Megharshini Created MFT.c

---
+++ OSLAB/MFT.c
@@ -0,0 +1,7 @@
+/* Name Of the Candidate: A. Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: MEMORY MANAGEMENT WITH FIXED PARTITIONING TECHNIQUE
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
+
