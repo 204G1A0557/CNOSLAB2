b2d440c99583dd81c54aa111883e3c5c05cd9934
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-10-11 12:07:25 +0530

    A Megharshini created FF.c

---
+++ OSLAB/FF.c
@@ -0,0 +1,7 @@
+/* Name Of the Candidate: A Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: First Fit contiguous memory allocation techniques
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
+
