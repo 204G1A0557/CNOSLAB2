23d0feec54469971e3f1efab89536601b62a690a
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-10-11 12:05:30 +0530

    A Megharshini created BF.c

---
+++ OSLAB/BF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A. Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: Best Fit contiguous memory allocation techniques
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
