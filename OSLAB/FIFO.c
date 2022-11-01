3ca908bbdcaa76e56eb91b150120e5bac6151920
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-11-01 12:33:03 +0530

    A Megharshini Created FIFO.c

---
+++ OSLAB/FIFO.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: FIFO page replacements algorithm
+   Date of Creation: 01-11-2022
+   Date of Execution: 01-11-2022
+*/
