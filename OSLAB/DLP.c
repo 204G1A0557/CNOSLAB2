4ef6e4a7723ae139ba8d4006a88c39beb64b955c
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-11-08 12:21:09 +0530

    Delete DLP.c

--- OSLAB/DLP.c
+++
@@ -1,6 +0,0 @@
-/* Name Of the Candidate: A. Megharshini
-   Roll No: 204G1A0557
-   Title of the Experiment: Banking Algorithn Dead Lock Prevention Program
-   Date of Creation: 08-11-2022
-   Date of Execution: 08-11-2022
-*/
