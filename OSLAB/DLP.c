7dcf4e2a643b8c36d078586129103a85b11a1cdf
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-11-08 12:22:30 +0530

    A Megharshini created DLP.c

---
+++ OSLAB/DLP.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: Banking Algorithm Dead Lock Prevention 
+   Date of Creation: 08-11-2022
+   Date of Execution: 08-11-2022
+*/
