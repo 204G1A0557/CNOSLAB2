0d814c97b89c7d5f06f784d94eb333e130b87be0
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-11-08 12:14:24 +0530

    A Megharshini Created DLA.c

---
+++ OSLAB/DLA.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A. Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: Bankers algorithm Dead Lock Avoidance Program
+   Date of Creation: 08-11-2022
+   Date of Execution: 08-11-2022
+*/
