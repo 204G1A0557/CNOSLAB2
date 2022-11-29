e2fbe4ab76879d18127992c034ab9f74d7853767
204G1A0557 <94538527+204G1A0557@users.noreply.github.com>
2022-11-29 13:10:54 +0530

    A.Megharshini Created OT.c

---
+++ OSLAB/OT.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: A.Megharshini
+   Roll No: 204G1A0557
+   Title of the Experiment: optimal page replacements algorithms.
+   Date of Creation: 29-11-2022
+   Date of Execution: 29-11-2022
+*/
